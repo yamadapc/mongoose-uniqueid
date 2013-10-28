e6ee8cf286cf769ab6a29f0cb229db6f4dfd78b0
yamadapc <tacla.yamada@gmail.com>
2013-10-28 20:34:28 -0200

    Implement more rudimentary tests - this still doesn't prove this module works.
    But it'll need to suffice.

--- test/index.test.js
+++ test/index.test.js
@@ -3,24 +3,27 @@
 var uniqueId = require('..'),
     mongoose = require('mongoose'),
     Schema   = mongoose.Schema,
-    should   = require('should');
-
-mongoose.connect('mongodb://localhost/test');
+    should   = require('should'),
+    qc       = require('quickcheck');
 
 describe('uniqueId', function() {
-  var Test;
+  var Test, doc;
+
+  // Connect to mongo
+  before(function() {
+    mongoose.connect('mongodb://localhost/test');
+  });
 
   // Register testing model
   before(function() {
     var TestSchema = new Schema({}, { _id: false });
-    TestSchema.plugin(uniqueId, {
+    TestSchema.plugin(uniqueId.plugin, {
       collections: ['Test']
     });
     Test = mongoose.model('Test', TestSchema);
   });
 
   describe('when creating a document', function() {
-    var doc;
     before(function() {
       doc = new Test();
       should.exist(doc);
@@ -37,6 +40,41 @@ describe('uniqueId', function() {
         doc.should.have.property('_id');
         done();
       });
+    });
+  });
+
+  describe('#isTaken', function() {
+    var fn;
+
+    it('should return a new function', function() {
+      fn = uniqueId._isTaken(doc._id, Test);
+      fn.should.be.instanceof(Function);
+    });
+
+    it('should pass on whether the id is taken to a callback', function(done) {
+      fn(function(err, result) {
+        should.not.exist(err);
+        should.exist(result);
+        result.should.equal(true);
+        done();
+      });
+    });
+  });
+
+  describe('#genUniqueId', function() {
+    var ids = [];
+    function propertyUnique() {
+      uniqueId._genUniqueId([Test], function(err, id) {
+        should.not.exist(err);
+        should.exist(id);
+        ids.should.not.contain(id);
+        ids.push(id);
+      });
+      return true;
+    }
+
+    it('should never pass on a duplicated id', function(done) {
+      try { qc.forAll(propertyUnique); } finally { done(); }
     });
   });
 });
